4f01bb6a80d2c4f08dd8309b68b2b4662cb55ae4
sirrui <gibsonruitiari@gmail.com>
2020-10-28 15:39:58 +0300

    added ui interface for the main and statistics fragment
    Signed-off-by: sirrui <gibsonruitiari@gmail.com>

---
+++ app/src/main/java/com/gibsoncodes/filio/features/activities/LauncherActivity.kt
@@ -0,0 +1,13 @@
+package com.gibsoncodes.filio.features.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.gibsoncodes.filio.R
+
+class LauncherActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+    }
+}
